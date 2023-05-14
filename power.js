a46e98d4028f6c4c8ebe661b7cc9d4c65c9ae0c7
Panchamic <133597882+Panchamic@users.noreply.github.com>
2023-05-14 11:44:03 -0700

    Create power.js

---
+++ power.js
@@ -0,0 +1,13 @@
+const menuItem = document.getElementById("menu_icon");
+const menu= document.getElementById("menu");
+
+menuIcon?.addEventListener("click",()=>{
+    if(menu?.className == "hidden"){
+        menu.classList.remove("hidden");
+    }
+    else {
+        menu.classList.add("hidden");
+    }
+    
+    
+});
